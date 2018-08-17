3815d1fd7abe31ee7a02d9a29ef192fc29b99bd8
Yoshie Takeda <35362557+dot-lab@users.noreply.github.com>
2018-08-17 11:14:10 +0900

    actionBarに選択したグループリストを表示する

--- app/src/main/java/xyz/dot_lab/prichandb/CoordinateListActivity.kt
+++ app/src/main/java/xyz/dot_lab/prichandb/CoordinateListActivity.kt
@@ -2,13 +2,22 @@ package xyz.dot_lab.prichandb
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.view.MenuItem
 
 class CoordinateListActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var intent = intent
         val selectedGroupName = intent.getStringExtra("groupName")
-        Log.d("CoordinateListActivity",selectedGroupName)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.title = selectedGroupName
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
