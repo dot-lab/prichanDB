c7261f2547f2c09154fbd073df95747495d62474
Yoshie Takeda <35362557+dot-lab@users.noreply.github.com>
2018-08-17 11:50:32 +0900

    コーディネートリストをDBから取得

--- app/src/main/java/xyz/dot_lab/prichandb/CoordinateListActivity.kt
+++ app/src/main/java/xyz/dot_lab/prichandb/CoordinateListActivity.kt
@@ -2,22 +2,42 @@ package xyz.dot_lab.prichandb
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.MenuItem
+import db.CoordinateDatabaseOpenHelper
+import db.ItemDataParser
+import entity.ItemData
+import org.jetbrains.anko.db.select
+import java.util.*
 
 class CoordinateListActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var intent = intent
         val selectedGroupName = intent.getStringExtra("groupName")
+        val coordinateList = getCoordinateList(selectedGroupName)
+
         val actionBar = supportActionBar
         if (actionBar != null) {
             actionBar.title = selectedGroupName
             actionBar.setDisplayHomeAsUpEnabled(true)
         }
+
+        Log.d("CoordinateListActivity",coordinateList[0].name)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         finish()
         return super.onOptionsItemSelected(item)
+    }
+
+    fun getCoordinateList(groupName: String): List<ItemData> {
+        val dbHelper = CoordinateDatabaseOpenHelper.getInstance(applicationContext)
+        var coordinateList: List<ItemData> = mutableListOf()
+        dbHelper.openDatabase()
+        dbHelper.use {
+           coordinateList = select(groupName).parseList(ItemDataParser())
+        }
+        return coordinateList
     }
 }
