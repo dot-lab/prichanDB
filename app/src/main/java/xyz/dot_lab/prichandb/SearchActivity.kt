9cbd71ab62276a8a0dc4af70b2f09c02fabd2cb3
Yoshie Takeda <35362557+dot-lab@users.noreply.github.com>
2018-09-06 14:30:13 +0900

    検索画面のレイアウトを作成

--- app/src/main/java/xyz/dot_lab/prichandb/SearchActivity.kt
+++ app/src/main/java/xyz/dot_lab/prichandb/SearchActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
-import org.jetbrains.anko.button
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import org.jetbrains.anko.*
 
 class SearchActivity: AppCompatActivity() {
-    val REQUEST_CODE: Int = 1
+    private val REQUEST_CODE: Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,10 +23,38 @@ class SearchActivity: AppCompatActivity() {
 
         verticalLayout {
             textView {
-                text = "hello,world"
+                text = "検索対象"
+            }
+            spinner {
+                val adapter = ArrayAdapter.createFromResource(applicationContext,R.array.GroupNames,android.R.layout.simple_spinner_item)
+                (adapter as ArrayAdapter<CharSequence>).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                this.adapter = adapter
+            }.onItemSelectedListener =  object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+                override fun onItemSelected(parent: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    val groupNameSpinner: Spinner = parent as Spinner
+                    val selected: String = groupNameSpinner.selectedItem.toString()
+                    toast(selected)
+                }
+            }
+
+            textView {
+                text = "表示対象"
+            }
+            radioGroup {
+                radioButton {
+                    text = "所持コーデのみ表示"
+                }
+                radioButton {
+                    text = "未所持コーデのみ表示"
+                }
+                radioButton {
+                    text = "すべて表示"
+                }
             }
             button {
-                text = "CoordinateListActivity"
+                text = "検索"
             }.setOnClickListener{
                 val intent = Intent(applicationContext,CoordinateListActivity::class.java)
                 startActivityForResult(intent,REQUEST_CODE)
